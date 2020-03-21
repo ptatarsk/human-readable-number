b5a4fbccdd7cc1078b2172b8a5f6aa80f089b649
TatarskiyPavel <Pasha.Tatarsky@gmal.com>
2020-03-21 19:08:07 +0200

    feat: done Human Readable Number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function toReadable (number) {
-  
-};
+  const readable_numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six',
+  'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
+  'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty', 'thirty',
+  'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let result = "0";
+
+  if (number < 20) {
+      result = readable_numbers[number];
+  } else if (number >= 20 && number <= 99) {
+      result = readable_numbers[Math.floor(number / 10) + 18];
+      if (number % 10)
+        result += ' ' + readable_numbers[number % 10];
+  } else if (number > 99) {
+    result = readable_numbers[Math.floor(number / 100)] + ' hundred';
+    if (number = Math.floor(number % 100)) {
+        if (number >= 20 && number <= 99) {
+            result += ' ' + readable_numbers[Math.floor(number / 10) + 18];
+            if (number % 10)
+                result += ' ' +  readable_numbers[number % 10];
+        } else {
+            result += ' ' + readable_numbers[number];
+        }
+    }
+  }
+
+  return result;
+}
